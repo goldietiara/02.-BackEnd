122fb77c6986f78e75289f0dcfbde359e9a4f173
goldietiara <goldietiara.acc@gmail.com>
2023-05-16 16:20:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let { penjumlahan, penggurangan } = require("./my_modules");
+
+console.log(penjumlahan(20, 7));
+console.log(penggurangan(10, 9));
---
+++ my_modules.js
@@ -0,0 +1,11 @@
+penjumlahan = (num1, num2) => {
+  return `${num1} plus ${num2} is ${num1 + num2}`;
+};
+penggurangan = (num1, num2) => {
+  return `${num1} minus ${num2} is ${num1 - num2}`;
+};
+
+module.exports = {
+  penjumlahan,
+  penggurangan,
+};
